2a54eb537164feb917c6f6393fbb7a1f2b2c9187
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-07-30 17:21:04 -0700

    Set up Plugin Info

---
+++ easy-promo-banner.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  Plugin Name:    Easy Promo Banner
+ *  Plugin URI:     https://github.com/microcurse/promo-banner
+ *  Description:    This simple WordPress plugin will display a promo banner at the top of the page or below the main navigation
+ *  Version:        0.1
+ *  Author:         Marc Reyes-Maninang
+ *  License:        MIT License
+ *  License URI:    https://www.gnu.org/licenses/gpl-2.0.html
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+?>
